693c4788a6b3cc13433ddb69432a7bb49fde0b11
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-01-14 14:51:35 +0530

    created a webservices for the employee details

---
+++ respository/day44-webservicesUses-dynamicDataVisualization/mywebserver/public/assignment-jquery-data/index.js
@@ -0,0 +1,94 @@
+var mainInputData = [];
+
+$.ajax({
+  url: "http://localhost:8082/assignment-jquery-data/information.json",
+  dataType: "JSON",
+  method: "GET",
+  data: {},
+  success: (response) => {
+    mainInputData = response.empInfo;
+    for (var i = 0; i < mainInputData.length; i++) {
+      superContentData(mainInputData[i]);
+    }
+  },
+  error: (error) => {
+    console.log(error);
+  },
+});
+
+var superContentData = (mainInputData) => {
+  var parentContainer = $(".parentContainer");
+  var creatingUlTag = $("<ul></ul>").attr("class", "ulTagData");
+
+  var heading = $("<h3></h3>").html("Employee Details").attr("class", "userInformationTitle")
+  creatingUlTag.append(heading)
+
+  var creatingNameList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee Name: " + mainInputData.name);
+  creatingUlTag.append(creatingNameList);
+
+  var creatingImgList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee Image: ");
+
+  var img = $("<img></img>")
+    .attr("src", mainInputData.img)
+    .attr("class", "imageDetails");
+
+  creatingImgList.append(img);
+  creatingUlTag.append(creatingImgList);
+
+  var creatingAgeList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee Age: " + mainInputData.age);
+  creatingUlTag.append(creatingAgeList);
+
+  var creatingGenderList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee gender: " + mainInputData.gender);
+  creatingUlTag.append(creatingGenderList);
+
+  var creatingContactList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee Contact: " + mainInputData.contact);
+  creatingUlTag.append(creatingContactList);
+
+  var creatingBasicList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee basicSallery: " + mainInputData.basicSallery);
+  creatingUlTag.append(creatingBasicList);
+
+  var mainSallery = mainInputData.basicSallery * 12;
+
+  var creatingHRAList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee HRA: " + (mainSallery * 50) / 100);
+  creatingUlTag.append(creatingHRAList);
+
+  var empTax;
+
+  var taxData = () => {
+    if (mainSallery <= 250000) {
+      empTax = "Don't need to pay any tax ";
+    } else if (mainSallery <= 500000) {
+      empTax = "You need to pay tax " + 0.05 * (mainSallery - 250000);
+    } else if (mainSallery <= 1000000) {
+      empTax =
+        "You need to pay tax " + (0.05 * 250000 + 0.2 * (mainSallery - 500000));
+    } else {
+      empTax =
+        "You need to pay tax " +
+        (0.05 * 250000 + 0.2 * 500000 + 0.3 * (mainSallery - 1000000));
+    }
+  };
+
+  taxData();
+
+  var creatingTaxList = $("<li></li>")
+    .attr("class", "list")
+    .html("Employee TAX: " + empTax);
+  creatingUlTag.append(creatingTaxList);
+
+  parentContainer.append(creatingUlTag);
+};
