1d659ae2271b2835e0b83834c825319a50dd1af8
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-01-21 21:44:43 +0530

    understanding the localStorage in details with new example

--- respository/day44-webservicesUses-dynamicDataVisualization/mywebserver/public/day48-localstorage-setGetMethod-localstorage/index.js
+++ respository/day44-webservicesUses-dynamicDataVisualization/mywebserver/public/day48-localstorage-setGetMethod-localstorage/index.js
@@ -4,6 +4,7 @@ var showDisplayData = () => {
   $("#mainData").show(3000);
 };
 
+var nameVal;
 var showData = (userName) => {
   nameVal = document.getElementById("mainName").innerHTML = userName;
   showDisplayData();
@@ -13,6 +14,15 @@ if((localStorage.getItem("userName")) != null){
   showData(localStorage.getItem("userName"));
 }
 
+var setTheme = (theme) => {
+  $("body").css("background", theme);
+  localStorage.setItem("themeColor", theme);
+};
+
+if (localStorage.getItem("themeColor") != null) {
+  setTheme(localStorage.getItem("themeColor"));
+}
+
 var saveData = document.getElementById("saveData");
 saveData.addEventListener("click", () => {
     userName = document.getElementById("inputData").value
@@ -20,4 +30,9 @@ saveData.addEventListener("click", () => {
     showData(userName);
 })
 
-var nameVal;
+var theme
+document.getElementById("themeData").addEventListener("click", () => {
+  theme = document.getElementById("themeData").value;
+  setTheme(theme)
+});
+
