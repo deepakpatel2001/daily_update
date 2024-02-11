b8f425c8f5073a0dc4e8e23932d4f15e9dee6768
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-02-11 21:31:02 +0530

    add scopes in javascript

---
+++ respository/episodes_anurag/scopes-in-js/index.js
@@ -0,0 +1,26 @@
+debugger
+const userName = "Deepak"
+console.log(userName);
+const age = 23;
+console.log(age);
+var password = 1234;
+console.log(password);
+
+function add(){
+    console.log(userName)
+    const a = 10;
+    const b = 3;
+    console.log(a+b);
+}
+
+add()
+function subtract(){
+    console.log(userName);
+    const userName = "Mohan"
+    console.log(userName);
+    const a = 24;
+    const b = 12;
+    console.log(a-b);
+}
+
+subtract()
