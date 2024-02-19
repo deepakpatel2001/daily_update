0db4f195cd0fe727e8e50caf6c20e95da766627d
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-02-19 17:51:51 +0530

    event listener

--- respository/episodes_anurag/spreadOperator-restParameter/index.js
+++ respository/episodes_anurag/spreadOperator-restParameter/index.js
@@ -1,14 +1,26 @@
-let container = document.querySelector(".container")
+// let container = document.querySelector(".container")
 
-for (let i = 1; i <= 100; i++) {
-    let images = document.createElement('img');
-    let paragraph = document.createElement('p');
-    images.setAttribute(
-        'src',
-        `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${i}.png`
-    );
+// for (let i = 1; i <= 100; i++) {
+//     let images = document.createElement('img');
+//     let paragraph = document.createElement('p');
+//     images.setAttribute(
+//         'src',
+//         `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${i}.png`
+//     );
 
-    paragraph.innerHTML = i;
-    container.append(images);
-    container.append(paragraph);
-}
+//     paragraph.innerHTML = i;
+//     container.append(images);
+//     container.append(paragraph);
+// }
+
+let parentContainer = document.querySelector('.container2');
+let cardAdder = document.querySelector('.cardAdder');
+
+let count = 0;
+cardAdder.addEventListener("click", () => {
+    count++
+    let newCard = document.createElement('div')
+    newCard.classList.add("card")
+    newCard.innerHTML = count
+    parentContainer.append(newCard)
+})
