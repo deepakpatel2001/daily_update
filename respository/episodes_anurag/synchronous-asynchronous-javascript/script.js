b439e9081c69ce0bb1b4d3e29a351a425059a610
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-02-22 22:36:45 +0530

    understood the synchronous and asynchronous with real world example

--- respository/episodes_anurag/synchronous-asynchronous-javascript/script.js
+++ respository/episodes_anurag/synchronous-asynchronous-javascript/script.js
@@ -10,6 +10,8 @@ randomDogs.addEventListener('click', () => {
         console.log(JSON.parse(xml.response).data.first_name);
     });
 
-    xml.open('GET', 'https://reqres.in/api/users/1?delay=5', false);
+    xml.open('GET', 'https://reqres.in/api/users/1?delay=5');
     xml.send();
+
+    console.log('jhello'); // if the open third parameter is false then the jhello will print after getting the data from the api, and another thing is if the third parameter is true then the jhello will print first and then teh api data will be print`
 });
