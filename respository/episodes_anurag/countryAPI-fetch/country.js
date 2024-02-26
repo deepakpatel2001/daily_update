1c91d58fec98c25a24d7330269f8166ccd052c57
Deepak Patel <115418592+deepakpatel2001@users.noreply.github.com>
2024-02-26 22:00:44 +0530

    country api data

---
+++ respository/episodes_anurag/countryAPI-fetch/country.js
@@ -0,0 +1,57 @@
+const urlParams = new URLSearchParams(location.search).get('name');
+const parentContainer = document.querySelector('.all-Data');
+const backBtn = document.querySelector('.back-button');
+const flag = document.querySelector('img');
+const countryName = document.querySelector('.countryName');
+const nativeName = document.querySelector('.native-name');
+const population = document.querySelector('.population');
+const region = document.querySelector('.region');
+const subRegion = document.querySelector('.sub-region');
+const capital = document.querySelector('.capital');
+const tld = document.querySelector('.tld');
+const currencies = document.querySelector('.currencies');
+const lang = document.querySelector('.lang');
+const borders = document.querySelector('.borderClassAnchor');
+
+backBtn.addEventListener('click', () => {
+    window.history.back(1);
+});
+
+async function myNewData() {
+    const url = `https://restcountries.com/v3.1/name/${urlParams}?fullText=true`;
+    const fetchData = await fetch(url);
+    const res = await fetchData.json();
+    res.forEach((data) => {
+        flag.src = `${data.flags.svg}`;
+        countryName.innerHTML = `${data.name.common}`;
+        population.innerHTML = `${data.population}`;
+        region.innerHTML = `${data.region}`;
+        subRegion.innerHTML = `${data.subregion}`;
+        capital.innerHTML = `${data.capital}`;
+        tld.innerHTML = `${Object.values(data.tld).join(', ')}`;
+        nativeName.innerHTML = `${
+            Object.values(data.name.nativeName)[0].official
+        }`;
+        currencies.innerHTML = `${Object.values(data.currencies)[0].name}`;
+        lang.innerHTML = `${Object.values(data.languages).join(', ')}`;
+        data.borders.forEach((border) => {
+            fetch(`https://restcountries.com/v3.1/alpha/${border}`)
+            .then((response) => response.json())
+            .then((countryBorder) => {
+                console.log(countryBorder.forEach((borderData) => {
+                    let achor = document.createElement('a');
+                    achor.className = 'borderValue';
+                    achor.innerHTML = borderData.name.common;
+                    achor.addEventListener('click', () => {
+                        achor.href = `./country.html?name=${borderData.name.common}`;
+                    })
+                    borders.append(achor);
+                }));
+                
+            } )
+            
+        });
+    });
+}
+
+myNewData();
---
+++ respository/episodes_anurag/countryAPI-fetch/index.js
@@ -0,0 +1,70 @@
+let cardContainer = document.querySelector('.cardContiner');
+let input = document.querySelector('input');
+let selectCountry = document.querySelector('.selectCountry');
+let darkMode = document.querySelector('.dark-mode');
+let bodyData = document.querySelector('body')
+
+darkMode.addEventListener('click', () => {
+    bodyData.classList.toggle('bodyData');
+})
+
+async function apiCalling() {
+    const mainUrl = 'https://restcountries.com/v3.1/independent?status=true';
+    const apiCall = await fetch(mainUrl);
+    const res = await apiCall.json();
+    res.forEach((country) => {
+        countryData(country);
+    });
+
+    selectCountry.addEventListener('change', (e) => {
+        fetch(`https://restcountries.com/v3.1/region/${selectCountry.value}`)
+            .then((res) => {
+                return res.json();
+            })
+            .then((regionData) => {
+                if (cardContainer.innerHTML !== '') {
+                    cardContainer.innerHTML = '';
+                }
+                regionData.forEach((regionCountry) => {
+                    countryData(regionCountry);
+                });
+            });
+    });
+
+    input.addEventListener('input', (e) => {
+        fetch('https://restcountries.com/v3.1/independent?status=true')
+            .then((res) => {
+                return res.json();
+            })
+            .then((searchData) => {
+                const searchCountryData = searchData.filter((searchCountry) => {
+                    return searchCountry.name.common.toLowerCase().includes(e.target.value.toLowerCase());
+                });
+                console.log(searchCountryData);
+                cardContainer.innerHTML = '';
+                searchCountryData.forEach((country) => {
+                    countryData(country);
+                });
+            });
+    });
+
+    function countryData(country) {
+        const countryCard = document.createElement('div');
+        countryCard.className = 'mainCard';
+        const anchorTag = document.createElement('a');
+        anchorTag.href = `./country.html?name=${country.name.common}`;
+        anchorTag.innerHTML = `<div class="imgUpper">
+                        <img src="${country.flags.svg}" alt="">
+                    </div>
+                    <div class="data">
+                        <h1>${country.name.common}</h1>
+                        <p><b>Population: </b>${country.population.toLocaleString()}</p>
+                        <p><b>Region: </b>${country.region}</p>
+                        <p><b>Capital: </b> ${country.capital}</p>
+                    </div>`;
+        countryCard.append(anchorTag);
+        cardContainer.append(countryCard);
+    }
+}
+
+apiCalling();
